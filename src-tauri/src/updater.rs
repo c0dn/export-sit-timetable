21c8f32d8dea377ffcff351d047cf84b03515b24
c0dn <19631981+c0dn@users.noreply.github.com>
2024-12-30 18:33:13 +0800

    Fix updater

--- src-tauri/src/updater.rs
+++ src-tauri/src/updater.rs
@@ -1,7 +1,14 @@
 use crate::models::GithubLatestReleaseRes;
 
 pub async fn get_latest_release() -> Result<String, String> {
-    let r = reqwest::get("https://api.github.com/repos/c0dn/export-sit-timetable/releases/latest")
+    let client = reqwest::Client::builder()
+        .gzip(true)
+        .brotli(true)
+        .deflate(true)
+        .user_agent("export-sit-timetable/1.1.0")
+        .build().unwrap();
+    let r = client.get("https://api.github.com/repos/c0dn/export-sit-timetable/releases/latest")
+        .send()
         .await
         .map_err(|_| "Version check failed: Network error")?;
     let latest_version = r
